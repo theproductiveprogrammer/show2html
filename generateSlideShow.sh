6e6ae83b6b16e4ff6ae5d8730024bbaed8e51633
Charles L <charles.lobo@gmail.com>
2021-09-21 11:18:13 +1000

    feat: add toggle and keyboard handlers to generator

--- generateSlideShow.sh
+++ generateSlideShow.sh
@@ -37,18 +37,24 @@ function genNav() {
   NAV="<style>.file-nav{text-align: left;}.file-nav a{ color: #444; text-decoration:none; padding-right: 2em;font: 12px sans-serif;}.file-nav a:hover{ color: #000; text-decoration: underline }</style>"
   NAV+="<div class=\"file-nav\">"
   if [[ "$PREV" != "" ]]; then
-    NAV+="<a href=\"$PREV\">prev</a> "
+    NAV+="<a id=\"prev\" href=\"$PREV\">(p)rev</a> "
   fi
   if [[ "$NEXT" != "" ]]; then
-    NAV+="<a href=\"$NEXT\">next</a> "
+    NAV+="<a id=\"next\" href=\"$NEXT\">(n)ext</a> "
   fi
+  NAV+="<a href=\"javascript:toggle()\">(t)oggle</a>"
   NAV+="</div>"
 }
 
+function genToggle() {
+  TOGGLE="let a_on = 0; function toggle() { const a = document.querySelectorAll('.delete'); const b = document.querySelectorAll('.insert'); let a_style; let b_style; if(a_on===1){a_style='display:none';b_style='';}else if(a_on===0){a_style='';b_style='display:none'}else{a_style='';bstyle='';};for(let i = 0;i < a.length;i++) a[i].style = a_style; for(let i = 0;i < b.length;i++) b[i].style = b_style; a_on = (++a_on % 2) ; };document.body.addEventListener('keydown'\, e => { if(e.key === 't') toggle(); else if(e.key === 'n') document.getElementById('next').click(); else if(e.key === 'p') document.getElementById('prev').click(); })"
+}
+
 function gdiff() {
+  genToggle
   genNav
   echo "generating diff of $1 in $CURR"
-  git show -U10000 $1 | show2html.sh | sed "s,</div></body>,$NAV</div></body>," > $CURR
+  git show -U10000 $1 | show2html.sh | sed "s,</div></body>,$NAV</div><script>$TOGGLE</script></body>," > $CURR
 }
 
 for COMMIT in "${COMMITS[@]}"
